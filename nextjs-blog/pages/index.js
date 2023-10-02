06473a110f1abe13f12691784adb779e7923d8ad
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2023-10-02 13:25:53 +0900

    페이지 소개문구 추가

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -398,6 +398,7 @@ export default function QuantPost({
                 <title>퀀트 종목 추천</title>
                 <link rel="icon" href="/images/icons8-algorithm-flatart-icons-lineal-color-32.png" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+                <meta name="description" content="한국주식에 대한 퀀트적인 분석과 필터링을 통해 적합한 종목을 추천하는 웹 페이지입니다. 효율적인 퀀트 투자 전략을 기반으로 한국 주식 시장에서 가치 있는 투자 대상을 찾아드립니다." />
             </Head>
             <Script
                 async
