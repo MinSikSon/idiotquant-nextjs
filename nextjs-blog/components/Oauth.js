d684f17eafdfd86afdf6853ff7e0cc9b985f2c5f
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2023-10-23 22:36:43 +0900

    layout 변경

--- nextjs-blog/components/Oauth.js
+++ nextjs-blog/components/Oauth.js
@@ -1,10 +1,12 @@
 import {
-    ArrowRightOnRectangleIcon
+    ArrowRightOnRectangleIcon, ChevronDownIcon
 } from "@heroicons/react/24/outline";
+import { Button, Menu, MenuHandler, MenuList } from "@material-tailwind/react";
 import Link from "next/link";
+import React from "react";
 
 export default function Oauth(props) {
-    const loginStatus = (!!props.loginStatus) ? `${props.loginStatus} 🖐` : '로그인';
+    const status = (!!props.loginStatus) ? `🖐 ${props.loginStatus}` : '로그인';
 
     const url = {
         pathname: '/login',
@@ -13,17 +15,29 @@ export default function Oauth(props) {
         },
     }
 
+    const [openMenu, setOpenMenu] = React.useState(false);
+
     return (
         <div className='text-black'>
             {!!props.loginStatus ?
-                <div className='flex'>
-                    <ArrowRightOnRectangleIcon strokeWidth={2} className="h-5 w-5" />
-                    <div className="pt-1 text-xs">{loginStatus}</div>
-                </div>
+                <Menu open={openMenu} handler={setOpenMenu} allowHover>
+                    <MenuHandler className='flex'>
+                        <div className="pt-1 text-xs">{status}</div>
+                        <ChevronDownIcon
+                            strokeWidth={2.5}
+                            className={`h-3.5 w-3.5 transition-transform ${openMenu ? "rotate-180" : ""
+                                }`}
+                        />
+                    </MenuHandler>
+                    <MenuList className="hidden w-[36rem] grid-cols-7 gap-3 overflow-visible lg:grid">
+                        <Button>계정설정</Button>
+                        <Button>로그아웃</Button>
+                    </MenuList>
+                </Menu>
                 :
                 <Link className='flex' href={url} passHref as='/login'>
                     <ArrowRightOnRectangleIcon strokeWidth={2} className="h-5 w-5" />
-                    <div className="pt-1 text-xs">{loginStatus}</div>
+                    <div className="pt-1 text-xs">{status}</div>
                 </Link>
             }
 
