d416b8f6df2298a81afbd19283556bce8779caf1
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2023-11-15 21:36:21 +0900

    적용

--- nextjs-blog/components/StocksOfInterestPanel.js
+++ nextjs-blog/components/StocksOfInterestPanel.js
@@ -1,5 +1,20 @@
+import { Tabs, Tab, TabsBody, TabsHeader } from "@material-tailwind/react";
+
 export default function StocksOfInterestPanel(props) {
+    if (false === props.stocksOfInterestPanelOpened) return <></>;
+
     return (<>
-        hihi
+        <Tabs value="ncav">
+            <TabsHeader
+                className="overflow-x-scroll rounded-none border-b border-blue-gray-100 bg-transparent p-0"
+                indicatorProps={{ className: "bg-transparent border-b-2 border-gray-900 shadow-none rounded-none", }}>
+                {props.stocksOfInterest.map(({ label, value }) => (
+                    <Tab className="shrink-0 w-fit text-sm" onClick={() => props.handleStocksOfInterestChange(value)} key={value} value={value}>
+                        {label}
+                    </Tab>
+                ))}
+            </TabsHeader>
+            <TabsBody></TabsBody>
+        </Tabs>
     </>);
 }
