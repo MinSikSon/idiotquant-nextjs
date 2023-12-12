2578a4d8e4d3cf57b5fb5bccdd614331c08d268d
MinSik Son <MinSikSon@users.noreply.github.com>
2023-12-13 01:53:11 +0900

    data 추가 (#128)
    Co-authored-by: minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>

--- nextjs-blog/pages/post_list.js
+++ nextjs-blog/pages/post_list.js
@@ -25,87 +25,77 @@ export default function PostList(props) {
             detail: ['좋은 기업을 좋은 가격에 사라'],
             img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Warren_Buffett_KU_Visit.jpg/400px-Warren_Buffett_KU_Visit.jpg'
         },
-        // {
-        //     title: '퀀트 용어',
-        //     subTitle: '🌵',
-        //     detail: [
-        //         '계량투자 (퀀트투자): 수치만 보고 투자하는 방식',
-        //         '투자스타일',
-        //         [
-        //             '밸류 전략: 가치투자',
-        //             '퀄리티 전략: 우량주투자',
-        //             '모멘텀 전략: 추세투자',
-        //             '자산배분 전략: 주식, 채권, 현금 등에 자산을 배분',
-        //             '콤보 전략: 위 4개 전략 조합',
-        //         ],
-        //         '매출 총액 - 매출 원가 = 매출 이익',
-        //         '매출 이익 - 판매 관리비 = 영업 이익',
-        //     ],
-        //     img: 'https://www.syesd.co.kr/homepage/syStoryImageFolder/1614817264026_fb8ff05cd5914f31981ff8aab95f8219_01.png'
-        // },
-        // {
-        //     title: '기본 용어',
-        //     subTitle: '🌵🌵',
-        //     detail: [
-        //         'CAGR: Compound Annual Growth Rate (연평균성장률)',
-        //         'NCAV: Net Current Asset Value (순유동자산)',
-        //         'MDD: Maximum Draw Down',
-        //         '투자자의 "최대 고통"을 측정하는 지표.',
-        //         '당기순이익: net income. 회사가 실제로 순수하게 남긴 이익',
-        //         'DIV: Dividend Yield (배당수익률)',
-        //         'BPS: Book-Per Share. 1 주 당 장부가',
-        //         'PER: Price Earning Ratio. 1 주 당 얻는 수익을 비율로 나타냄.',
-        //         'PER * EPS = 1 주 가격',
-        //         'EPS: Earning Per Share. 1 주 당 수익금',
-        //         'PBR: Price Book Ratio. 장부가 대비 1주 가격을 비율로 나타냄',
-        //         'EV/EBIT: 몇 년 만에 투자 비용 회수할 수 있는지 알려주는 지표.',
-        //         'EV: Enterprise Value. 기업가치. 실제 얼마를 투입하면 기업을 완전히 지배할 수 있는지 보여주는 지표.',
-        //         [
-        //             'EV = 시가총액 + 부채 - 현금 - 비영업자산'
-        //         ],
-        //         'EBIT: Earning Before Interest and Taxes. 영업이익. 이자비용과 법인세 차감 전 이익.',
-        //         'ROC: Return On Capital. ROC = EBIT(영업이익)/투자자본',
-        //         '투자자본 = 고정자산+유동자산-유동부채',
-        //         '고정자산 == 비유동자산',
-        //         'GP/A = 매출총이익/총자산 = (매출액 - 매출원가) / (자기자본 + 대출(부채))',
-        //         [
-        //             'GP/A: GP: Gross Prifit. 매출 총 이익. A: Assets. 총 자산.'
-        //         ],
-        //         'BS: Balance Sheet. 재무 상태표(대차 대조표)',
-        //     ],
-        //     img: 'https://www.syesd.co.kr/homepage/syStoryImageFolder/1614817264026_fb8ff05cd5914f31981ff8aab95f8219_01.png'
-        // },
-
+        {
+            title: '퀀트 용어',
+            subTitle: '🌵',
+            detail: [
+                '계량투자 (퀀트투자): 수치만 보고 투자하는 방식',
+                '투자스타일',
+                [
+                    '밸류 전략: 가치투자',
+                    '퀄리티 전략: 우량주투자',
+                    '모멘텀 전략: 추세투자',
+                    '자산배분 전략: 주식, 채권, 현금 등에 자산을 배분',
+                    '콤보 전략: 위 4개 전략 조합',
+                ],
+                '매출 총액 - 매출 원가 = 매출 이익',
+                '매출 이익 - 판매 관리비 = 영업 이익',
+            ],
+            img: 'https://www.syesd.co.kr/homepage/syStoryImageFolder/1614817264026_fb8ff05cd5914f31981ff8aab95f8219_01.png'
+        },
+        {
+            title: '기본 용어',
+            subTitle: '🌵🌵',
+            detail: [
+                'CAGR: Compound Annual Growth Rate (연평균성장률)',
+                'NCAV: Net Current Asset Value (순유동자산)',
+                'MDD: Maximum Draw Down',
+                '투자자의 "최대 고통"을 측정하는 지표.',
+                '당기순이익: net income. 회사가 실제로 순수하게 남긴 이익',
+                'DIV: Dividend Yield (배당수익률)',
+                'BPS: Book-Per Share. 1 주 당 장부가',
+                'PER: Price Earning Ratio. 1 주 당 얻는 수익을 비율로 나타냄.',
+                'PER * EPS = 1 주 가격',
+                'EPS: Earning Per Share. 1 주 당 수익금',
+                'PBR: Price Book Ratio. 장부가 대비 1주 가격을 비율로 나타냄',
+                'EV/EBIT: 몇 년 만에 투자 비용 회수할 수 있는지 알려주는 지표.',
+                'EV: Enterprise Value. 기업가치. 실제 얼마를 투입하면 기업을 완전히 지배할 수 있는지 보여주는 지표.',
+                [
+                    'EV = 시가총액 + 부채 - 현금 - 비영업자산'
+                ],
+                'EBIT: Earning Before Interest and Taxes. 영업이익. 이자비용과 법인세 차감 전 이익.',
+                'ROC: Return On Capital. ROC = EBIT(영업이익)/투자자본',
+                '투자자본 = 고정자산+유동자산-유동부채',
+                '고정자산 == 비유동자산',
+                'GP/A = 매출총이익/총자산 = (매출액 - 매출원가) / (자기자본 + 대출(부채))',
+                [
+                    'GP/A: GP: Gross Prifit. 매출 총 이익. A: Assets. 총 자산.'
+                ],
+                'BS: Balance Sheet. 재무 상태표(대차 대조표)',
+            ],
+            img: 'https://www.syesd.co.kr/homepage/syStoryImageFolder/1614817264026_fb8ff05cd5914f31981ff8aab95f8219_01.png'
+        },
+        {
+            title: '기타 용어',
+            subTitle: '🌵🌵🌵',
+            detail: [
+                '매출 총액 - 매출 원가 = 매출 이익',
+                '매출 이익 - 판매 관리비 = 영업 이익',
+                '영업 이익 - 영업 이외의 활동으로 인한 손익 = 순이익',
+                '영업 이외의 활동으로 인한 손익: 부동산, 증권, 환차 등의 손익',
+                '상장 폐지 대상: 2013년 이후부터, 5년 연속 영업 이익 적자 기업.',
+                '참고: IFRS 에서는 영업 이익에 1회성 처분 비용 포함 시키기도 함.',
+                '제159조(사업보고서 등의 제출)',
+                [
+                    '① 주권상장법인, 그 밖에 대통령령으로 정하는 법인(이하 “사업보고서 제출대상법인”이라 한다)은 그 사업보고서를 각 사업연도 경과 후 90일 이내에 금융위원회와 거래소에 제출하여야 한다.',
+                    '자본시장과 금융투자업에 관한 법률',
+                ]
+            ],
+            img: 'https://www.syesd.co.kr/homepage/syStoryImageFolder/1614817264026_fb8ff05cd5914f31981ff8aab95f8219_01.png'
+        },
     ]);
 
     const [selectedContentIndex, setSelectedContentIndex] = React.useState(-1);
-    // const Etc = () => {
-    //     return (
-    //         <>
-    //             <ul className='list-disc list-inside pb-4 text-xs'>
-
-    //             </ul >
-    //             <ul className='list-disc list-inside bg-gray-200 pb-4 text-xs'>
-    //                 <li>매출 총액 - 매출 원가 = 매출 이익</li>
-    //                 <li>매출 이익 - 판매 관리비 = 영업 이익</li>
-    //                 <li>영업 이익 - 영업 이외의 활동으로 인한 손익 = 순이익</li>
-    //                 <li>영업 이외의 활동으로 인한 손익: 부동산, 증권, 환차 등의 손익</li>
-    //                 <li>상장 폐지 대상: 2013년 이후부터, 5년 연속 영업 이익 적자 기업.</li>
-    //                 <li>참고: IFRS 에서는 영업 이익에 1회성 처분 비용 포함 시키기도 함.</li>
-    //             </ul>
-
-    //             <ul className='list-disc list-inside bg-gray-300 pb-4 text-xs'>
-    //                 <li>제159조(사업보고서 등의 제출)</li>
-    //                 <li className="pl-4">① 주권상장법인, 그 밖에 대통령령으로 정하는 법인(이하 “사업보고서 제출대상법인”이라 한다)은 그 사업보고서를 각 사업연도 경과 후 90일 이내에 금융위원회와 거래소에 제출하여야 한다.</li>
-    //                 <li className="pl-4"><a href='https://www.law.go.kr/법령/자본시장과%20금융투자업에%20관한%20법률/제159조'>자본시장과 금융투자업에 관한 법률</a></li>
-    //             </ul>
-
-    //             <ul className='list-disc list-inside bg-gray-300 pb-4 text-xs'>
-    //                 <li>제159조(사업보고서 등의 제출)</li>
-    //             </ul>
-    //         </>
-    //     );
-    // }
 
     function TitlePanel(props) {
         return (<ListItem className='text-black'>
@@ -121,52 +111,46 @@ export default function PostList(props) {
         </ListItem>);
     }
 
+    const Article = (props) => {
+        const item = props.item;
+        const index = props.index;
+        const selectedContentIndex = props.selectedContentIndex;
+        const setSelectedContentIndex = props.setSelectedContentIndex;
+
+        // console.log(`selectedContentIndex`, selectedContentIndex, `item`, item);
+
+        return (
+            <div onClick={() => setSelectedContentIndex(index == selectedContentIndex ? -1 : index)}>
+                {(selectedContentIndex == index) ?
+                    <TemplateArticle
+                        title={item.title}
+                        subTitle={item.subTitle}
+                        link={item.link}
+                        detail={item.detail}
+                        img1={item.img}
+                    />
+                    : <TemplateArticleSimple
+                        title={item.title}
+                        subTitle={item.subTitle}
+                        link={item.link}
+                        img1={item.img}
+                    />}
+            </div>
+        );
+    }
+
     return (
         <>
             <TitlePanel />
-            <List>
-                {contents.map((item, index) => {
-                    console.log(`selectedContentIndex`, selectedContentIndex, `item`, item);
-                    return (
-                        <div onClick={() => setSelectedContentIndex(index == selectedContentIndex ? -1 : index)}>
-                            {(selectedContentIndex == index) ?
-                                <TemplateArticle
-                                    key={index}
-                                    title={item.title}
-                                    subTitle={item.subTitle}
-                                    link={item.link}
-                                    detail={item.detail}
-                                    img1={item.img}
-                                />
-                                : <TemplateArticleSimple
-                                    key={`${index}_Simple`}
-                                    title={item.title}
-                                    subTitle={item.subTitle}
-                                    link={item.link}
-                                    img1={item.img}
-                                />}
-                        </div>
-                    );
-                })}
-
-                {/* <Link href='./posts/quant'>
-                    <ListItem className='border-b-2'>
-                        <ListItemPrefix>
-                            <CurrencyDollarIcon strokeWidth={2} className="h-6 w-6" />
-                        </ListItemPrefix>
-                        <div className='pt-1'>퀀트 용어</div>
-                    </ListItem>
-                </Link> */}
-                <Link href='./posts/etc'>
-                    <ListItem className='border-b-2'>
-                        <ListItemPrefix>
-                            <ChatBubbleOvalLeftEllipsisIcon strokeWidth={2} className="h-6 w-6" />
-                        </ListItemPrefix>
-                        <div className='pt-1'>기타 등등 글</div>
-                    </ListItem>
-                </Link>
-            </List>
-
+            {contents.map((item, index) =>
+                <Article
+                    key={index.toString()}
+                    index={index}
+                    item={item}
+                    selectedContentIndex={selectedContentIndex}
+                    setSelectedContentIndex={setSelectedContentIndex}
+                />
+            )}
         </>
     );
 }
