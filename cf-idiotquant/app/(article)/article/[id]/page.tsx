a7e4f11057af12ee975626263745f7631579fe55
MinSik Son <MinSikSon@users.noreply.github.com>
2025-02-02 00:22:05 +0900

    186 feature strategy list (#188)
    * 오타 수정
    
    * design fix
    
    * clean code
    
    * 조건 filter 기능 추가
    
    * 전략 filter 추가
    
    * 전략 Filter 추가 작업 1
    
    * 전략 filter 추가 작업 2
    
    * 전략 filter 추가 작업 완료
    
    * alert 제거
    
    * error fix
    
    ---------
    
    Co-authored-by: minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>

--- cf-idiotquant/app/(article)/article/[id]/page.tsx
+++ cf-idiotquant/app/(article)/article/[id]/page.tsx
@@ -8,8 +8,8 @@ export default function Item({ params: { id } }: { params: { id: number } }) {
     const articleList = useAppSelector(selectArticleList);
 
     return <TemplateArticle
-        title={articleList[id].name}
-        subTitle={articleList[id].desc}
+        title={articleList[id].title}
+        subTitle={articleList[id].subTitle}
         // link={articleList[id].link}
         detail={articleList[id].detail}
         img={articleList[id].imgs}
