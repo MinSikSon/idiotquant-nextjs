aab75cb4ea8214ad3bb0ce78bd0814ff7f842d9a
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2025-02-08 23:48:04 +0900

    몇 가지 필터 조건 추가

--- cf-idiotquant/app/(backtest)/backtest/page.tsx
+++ cf-idiotquant/app/(backtest)/backtest/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
-import { Button, ButtonGroup } from "@material-tailwind/react";
+import { Button, ButtonGroup, Input } from "@material-tailwind/react";
 import RegisterTemplate from "@/components/register_template";
 import Link from "next/link";
 
@@ -14,15 +14,26 @@ import {
     TimelineBody,
     Typography,
 } from "@material-tailwind/react";
+import { setBackTestConditionType1, getBackTestConditionType1 } from "@/lib/features/backtest/backtestSlice";
+import { setBackTestConditionType2, getBackTestConditionType2 } from "@/lib/features/backtest/backtestSlice";
+import { setBackTestConditionType3, getBackTestConditionType3 } from "@/lib/features/backtest/backtestSlice";
+import React from "react";
 
 export default function BackTest() {
-    // const dispatch = useAppDispatch();
+    const dispatch = useAppDispatch();
+
+    const backTestConditionType1 = useAppSelector(getBackTestConditionType1);
+    const backTestConditionType2 = useAppSelector(getBackTestConditionType2);
+    const backTestConditionType3 = useAppSelector(getBackTestConditionType3);
+
     const getTitle = () => {
         return "back test"
-    }
+    };
+
     const getSubTitle = () => {
         return "back test gogo"
-    }
+    };
+
     const getStepContents = (title: string, list: any[], selectValue: any, handleOnClick: any) => {
         return <div className="flex flex-col">
             <div className="flex flex-col justify-between items-left mt-2 text-black hover:text-blue-500">
@@ -32,23 +43,51 @@ export default function BackTest() {
                 </ButtonGroup>
             </div>
         </div>
-    }
+    };
 
-    const handleOnClick = () => {
-        console.log(`[handleOnClick] do nothing`);
-    }
-    // 1. 특정 날짜 기준으로 종목 뽑기
-    // - strategy-register 참고
-    // 2. 해당 종목을 팔기
-    // - 현재 가격으로 비교
-    // { getStepContents("전략", ["test"], "test", (item: any) => { dispatch(setDefaultStrategy(item)); }) }
-    return <>
-        <RegisterTemplate
-            cardBodyFix={true}
-            title={getTitle()}
-            subTitle={getSubTitle()}
-            content={getStepContents("전략", ["test"], "test", (item: any) => { console.log(`do nothing`, item); })}
-            footer={
+    const getDateContents = (title: string, selectValue: any, handleOnChange: any) => {
+        return <div className="flex flex-col">
+            <div className="flex flex-col justify-between items-left mt-2 text-black hover:text-blue-500">
+                <Input
+                    className=""
+                    color="black"
+                    label={title}
+                    type="date"
+                    value={selectValue} crossOrigin={undefined}
+                    onChange={(e) => handleOnChange(e.currentTarget.value)}
+                    min="2004-04-01"
+                    max="2024-04-01"
+                />
+            </div>
+        </div>
+    };
+
+    React.useEffect(() => {
+        console.log(`backTestConditionType1`, backTestConditionType1);
+    }, [backTestConditionType1]);
+    React.useEffect(() => {
+        console.log(`backTestConditionType2`, backTestConditionType2);
+    }, [backTestConditionType2]);
+    React.useEffect(() => {
+        console.log(`backTestConditionType3`, backTestConditionType3);
+    }, [backTestConditionType3]);
+
+    const getContents = () => {
+        return <>
+            {getStepContents(backTestConditionType1.title, backTestConditionType1.strategyList, backTestConditionType1.strategy, (item: string) => { dispatch(setBackTestConditionType1({ ...backTestConditionType1, strategy: item })); })}
+            {getDateContents(backTestConditionType2.title, backTestConditionType2.startDate, (item: any) => { dispatch(setBackTestConditionType2({ ...backTestConditionType2, startDate: item })); })}
+            {getDateContents(backTestConditionType3.title, backTestConditionType3.endDate, (item: any) => { dispatch(setBackTestConditionType3({ ...backTestConditionType3, endDate: item })); })}
+        </>
+    };
+
+    const getFooter = () => {
+
+        const handleOnClick = () => {
+            console.log(`[handleOnClick] do nothing`);
+        }
+
+        return <>
+            {
                 <div className="flex flex-col gap-2">
                     <div className="flex justify-between items-center">
                         <Link href={`/`}>
@@ -65,13 +104,27 @@ export default function BackTest() {
                     </div>
                 </div>
             }
+        </>
+    };
+
+    // 1. 특정 날짜 기준으로 종목 뽑기
+    // - strategy-register 참고
+    // 2. 해당 종목을 팔기
+    // - 현재 가격으로 비교
+    // { getStepContents("전략", ["test"], "test", (item: any) => { dispatch(setDefaultStrategy(item)); }) }
+    return <>
+        <RegisterTemplate
+            cardBodyFix={true}
+            title={getTitle()}
+            subTitle={getSubTitle()}
+            content={getContents()}
+            footer={getFooter()}
         />
         <div className="p-2 m-2 border rounded">
             <DefaultTimeline />
         </div>
     </>
 }
-
 
 const DefaultTimeline = () => {
     return (
--- cf-idiotquant/lib/features/backtest/backtestSlice.tsx
+++ cf-idiotquant/lib/features/backtest/backtestSlice.tsx
@@ -1,26 +1,69 @@
 import { createAppSlice } from "@/lib/createAppSlice";
+import { PayloadAction } from "@reduxjs/toolkit";
+
+export interface BackTestConditionType1 {
+    state: string;
+    title: string;
+    strategyList: string[];
+    strategy: string;
+}
+export interface BackTestConditionType2 {
+    state: string;
+    title: string;
+    startDate: string;
+}
+export interface BackTestConditionType3 {
+    state: string;
+    title: string;
+    endDate: string;
+}
 
 interface BackTestInfo {
     state: "init"
-    |""
+    | "type1"
+    | "type2"
+    | "type3"
     ;
-    startFinancialInfo: any;
-    startMarketInfo: any;
-    endMarketInfo: any;
-    value: any;
+    backTestConditionType1: BackTestConditionType1;
+    backTestConditionType2: BackTestConditionType2;
+    backTestConditionType3: BackTestConditionType3;
 }
 
 const initialState: BackTestInfo = {
     state: "init",
-    startFinancialInfo: {},
-    startMarketInfo: {},
-    endMarketInfo: {},
-    value: {},
+    backTestConditionType1: {
+        state: "init",
+        title: "전략",
+        strategyList: ["none", "NCAV"],
+        strategy: "none",
+    },
+    backTestConditionType2: {
+        state: "init",
+        title: "시작날짜",
+        startDate: "2004-04-01",
+    },
+    backTestConditionType3: {
+        state: "init",
+        title: "종료날짜",
+        endDate: "2024-04-01",
+    }
 }
 export const backtestSlice = createAppSlice({
     name: "backtest",
     initialState,
     reducers: (create) => ({
+        setBackTestConditionType1: create.reducer((state, action: PayloadAction<BackTestConditionType1>) => {
+            state.backTestConditionType1 = action.payload;
+            state.state = "type1";
+        }),
+        setBackTestConditionType2: create.reducer((state, action: PayloadAction<BackTestConditionType2>) => {
+            state.backTestConditionType2 = action.payload;
+            state.state = "type2";
+        }),
+        setBackTestConditionType3: create.reducer((state, action: PayloadAction<BackTestConditionType3>) => {
+            state.backTestConditionType3 = action.payload;
+            state.state = "type3";
+        })
         // getStartFinancialInfo: create.asyncThunk(
         //     async ({ year, quarter }: { year: string, quarter: string }) => {
         //         // console.log(`[getStartFinancialInfo]`, year, quarter);
@@ -43,69 +86,69 @@ export const backtestSlice = createAppSlice({
         //         }
         //     },
         // ),
-    //     getStartMarketInfo: create.asyncThunk(
-    //         async ({ date }: { date: string }) => {
-    //             console.log(`[getStartMarketInfo]`, date);
-    //             const res: any = await getMarketInfo(date);
-    //             return res;
-    //         },
-    //         {
-    //             pending: (state) => {
-    //                 // console.log(`[getStartMarketInfo] pending`);
-    //                 state.state = "loading";
-    //             },
-    //             fulfilled: (state, action) => {
-    //                 // console.log(`[getStartMarketInfo] fulfilled - action.payload:`, action.payload);
-    //                 state.state = "loaded";
-    //                 state.startMarketInfo = action.payload;
-    //             },
-    //             rejected: (state) => {
-    //                 // console.log(`[getStartMarketInfo] rejected`);
-    //                 state.state = "get-rejected";
-    //             }
-    //         },
-    //     ),
-    //     getEndMarketInfo: create.asyncThunk(
-    //         async ({ date }: { date: string }) => {
-    //             // console.log(`[getEndMarketInfo]`, date);
-    //             const res: any = await getMarketInfo(date);
-    //             return res;
-    //         },
-    //         {
-    //             pending: (state) => {
-    //                 // console.log(`[getEndMarketInfo] pending`);
-    //                 state.state = "loading";
-    //             },
-    //             fulfilled: (state, action) => {
-    //                 // console.log(`[getEndMarketInfo] fulfilled - action.payload:`, action.payload);
-    //                 state.state = "loaded";
-    //                 state.endMarketInfo = action.payload;
-    //             },
-    //             rejected: (state) => {
-    //                 // console.log(`[getEndMarketInfo] rejected`);
-    //                 state.state = "get-rejected";
-    //             }
-    //         },
-    //     ),
-    //     setBackTestStrategyList: create.asyncThunk(
-    //         async ({ financialInfoDate, marketInfoDate, ncavList }: { financialInfoDate: string, marketInfoDate: string, ncavList: string }) => {
-    //             const res: any = await setNcavList(financialInfoDate, marketInfoDate, ncavList);
-    //             return res;
-    //         },
-    //         {
-    //             pending: (state) => {
-    //                 state.state = "loading";
-    //             },
-    //             fulfilled: (state, action) => {
-    //                 state.state = "loaded";
-    //                 const json = JSON.parse(action.payload);
-    //                 state.value = json;
-    //             },
-    //             rejected: (state) => {
-    //                 state.state = "set-rejected";
-    //             }
-    //         },
-    //     )
+        //     getStartMarketInfo: create.asyncThunk(
+        //         async ({ date }: { date: string }) => {
+        //             console.log(`[getStartMarketInfo]`, date);
+        //             const res: any = await getMarketInfo(date);
+        //             return res;
+        //         },
+        //         {
+        //             pending: (state) => {
+        //                 // console.log(`[getStartMarketInfo] pending`);
+        //                 state.state = "loading";
+        //             },
+        //             fulfilled: (state, action) => {
+        //                 // console.log(`[getStartMarketInfo] fulfilled - action.payload:`, action.payload);
+        //                 state.state = "loaded";
+        //                 state.startMarketInfo = action.payload;
+        //             },
+        //             rejected: (state) => {
+        //                 // console.log(`[getStartMarketInfo] rejected`);
+        //                 state.state = "get-rejected";
+        //             }
+        //         },
+        //     ),
+        //     getEndMarketInfo: create.asyncThunk(
+        //         async ({ date }: { date: string }) => {
+        //             // console.log(`[getEndMarketInfo]`, date);
+        //             const res: any = await getMarketInfo(date);
+        //             return res;
+        //         },
+        //         {
+        //             pending: (state) => {
+        //                 // console.log(`[getEndMarketInfo] pending`);
+        //                 state.state = "loading";
+        //             },
+        //             fulfilled: (state, action) => {
+        //                 // console.log(`[getEndMarketInfo] fulfilled - action.payload:`, action.payload);
+        //                 state.state = "loaded";
+        //                 state.endMarketInfo = action.payload;
+        //             },
+        //             rejected: (state) => {
+        //                 // console.log(`[getEndMarketInfo] rejected`);
+        //                 state.state = "get-rejected";
+        //             }
+        //         },
+        //     ),
+        //     setBackTestStrategyList: create.asyncThunk(
+        //         async ({ financialInfoDate, marketInfoDate, ncavList }: { financialInfoDate: string, marketInfoDate: string, ncavList: string }) => {
+        //             const res: any = await setNcavList(financialInfoDate, marketInfoDate, ncavList);
+        //             return res;
+        //         },
+        //         {
+        //             pending: (state) => {
+        //                 state.state = "loading";
+        //             },
+        //             fulfilled: (state, action) => {
+        //                 state.state = "loaded";
+        //                 const json = JSON.parse(action.payload);
+        //                 state.value = json;
+        //             },
+        //             rejected: (state) => {
+        //                 state.state = "set-rejected";
+        //             }
+        //         },
+        //     )
     }),
     selectors: {
         // selectStartFinancialInfo: (state) => state.startFinancialInfo,
@@ -113,5 +156,14 @@ export const backtestSlice = createAppSlice({
         // selectEndMarketInfo: (state) => state.endMarketInfo,
         // selectBackTestState: (state) => state.state,
         // selectBackTestNcavList: (state) => state.value,
+        getBackTestConditionType1: (state) => state.backTestConditionType1,
+        getBackTestConditionType2: (state) => state.backTestConditionType2,
+        getBackTestConditionType3: (state) => state.backTestConditionType3,
     }
 });
+export const { setBackTestConditionType1 } = backtestSlice.actions;
+export const { getBackTestConditionType1 } = backtestSlice.selectors;
+export const { setBackTestConditionType2 } = backtestSlice.actions;
+export const { getBackTestConditionType2 } = backtestSlice.selectors;
+export const { setBackTestConditionType3 } = backtestSlice.actions;
+export const { getBackTestConditionType3 } = backtestSlice.selectors;
