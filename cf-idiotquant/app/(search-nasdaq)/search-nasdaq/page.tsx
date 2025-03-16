fe42b6e7ceeb50c2dae0c77cc87e3e1ecc7144d1
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2025-03-16 10:24:05 +0900

    clean code

--- cf-idiotquant/app/(search-nasdaq)/search-nasdaq/page.tsx
+++ cf-idiotquant/app/(search-nasdaq)/search-nasdaq/page.tsx
@@ -6,7 +6,7 @@ import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { selectLoginState } from "@/lib/features/login/loginSlice";
 import { usePathname } from "next/navigation";
 import { isValidCookie } from "@/components/util";
-import { getKoreaInvestmentUsMaretSearchInfo, reqGetQuotationsSearchInfo } from "@/lib/features/koreaInvestmentUsMarket/koreaInvestmentUsMarketSlice";
+import { getKoreaInvestmentUsMaretSearchInfo, KoreaInvestmentOverseasPriceDetail, KoreaInvestmentOverseasPriceDetailOutput, KoreaInvestmentOverseasSearchInfo, KoreaInvestmentOverseasSearchInfoOutput, reqGetQuotationsSearchInfo } from "@/lib/features/koreaInvestmentUsMarket/koreaInvestmentUsMarketSlice";
 import { getKoreaInvestmentUsMaretPriceDetail, reqGetQuotationsPriceDetail } from "@/lib/features/koreaInvestmentUsMarket/koreaInvestmentUsMarketSlice";
 import { getKoreaInvestmentToken, KoreaInvestmentToken, reqGetInquireBalance } from "@/lib/features/koreaInvestment/koreaInvestmentSlice";
 import SearchAutocomplete from "@/components/searchAutoComplete";
@@ -25,11 +25,12 @@ export default function Search() {
 
     const kiToken: KoreaInvestmentToken = useAppSelector(getKoreaInvestmentToken);
 
-    const kiUsMaretSearchInfo: any = useAppSelector(getKoreaInvestmentUsMaretSearchInfo);
-    const kiUsMaretPriceDetail: any = useAppSelector(getKoreaInvestmentUsMaretPriceDetail);
+    const kiUsMaretSearchInfo: KoreaInvestmentOverseasSearchInfo = useAppSelector(getKoreaInvestmentUsMaretSearchInfo);
+    const kiUsMaretPriceDetail: KoreaInvestmentOverseasPriceDetail = useAppSelector(getKoreaInvestmentUsMaretPriceDetail);
 
     React.useEffect(() => {
         if (DEBUG) console.log(`[Search]`, `kiToken:`, kiToken);
+        if (DEBUG) console.log(`[Search]`, `loginState:`, loginState);
         if ("cf" == loginState || "kakao" == loginState) {
             const isValidKiAccessToken = !!kiToken["access_token"];
             if (true == isValidKiAccessToken) {
@@ -45,7 +46,9 @@ export default function Search() {
         if (DEBUG) console.log(`React.useEffect [kiUsMaretPriceDetail]`, kiUsMaretPriceDetail);
     }, [kiUsMaretPriceDetail])
 
-    if ("init" == loginState || "rejected" == loginState) {
+    if (DEBUG) console.log(`kiUsMaretSearchInfo`, kiUsMaretSearchInfo);
+    if (DEBUG) console.log(`kiUsMaretPriceDetail`, kiUsMaretPriceDetail);
+    if ("init" == loginState || "rejected" == loginState || "pending" == loginState) {
         return <>
             <Login parentUrl={pathname} />
         </>;
@@ -63,22 +66,24 @@ export default function Search() {
         dispatch(reqGetQuotationsPriceDetail({ koreaInvestmentToken: kiToken, PDNO: stockName }));
     }
 
-    if (!!!kiUsMaretSearchInfo.output && !!!kiUsMaretPriceDetail.output) {
+    const kiUsMaretSearchInfoOutput: KoreaInvestmentOverseasSearchInfoOutput = kiUsMaretSearchInfo.output;
+    const kiUsMaretPriceDetailOutput: KoreaInvestmentOverseasPriceDetailOutput = kiUsMaretPriceDetail.output;
+    if (!!!kiUsMaretSearchInfoOutput && !!!kiUsMaretPriceDetailOutput) {
         <SearchAutocomplete onSearchButton={onSearchButton} validCorpNameArray={nasdaq_tickers} />
     }
 
     return <>
         <SearchAutocomplete onSearchButton={onSearchButton} validCorpNameArray={nasdaq_tickers} />
-        <div className="font-mono">{kiUsMaretSearchInfo.output.prdt_name} ({kiUsMaretSearchInfo.output.prdt_eng_name})</div>
+        <div className="font-mono">{kiUsMaretSearchInfoOutput.prdt_name} ({kiUsMaretSearchInfoOutput.prdt_eng_name})</div>
         <div>
             {/* <div className="text-xs">/uapi/overseas-price/v1/quotations/search-info</div> */}
             {/* <div className="text-[0.5rem]">{JSON.stringify(kiUsMaretSearchInfo)}</div> */}
-            <div className="text-[0.5rem]">{Object.keys(kiUsMaretSearchInfo.output).map((key: any) => { return <div key={key}>{key} : {kiUsMaretSearchInfo.output[key]}</div> })}</div>
+            <div className="text-[0.5rem]">{Object.keys(kiUsMaretSearchInfoOutput).map(key => { return <div key={key}>{key}:{kiUsMaretSearchInfoOutput[key as keyof typeof kiUsMaretSearchInfoOutput]}</div> })}</div>
         </div>
         <div>
             {/* <div className="text-xs">/uapi/overseas-price/v1/quotations/price-detail</div> */}
             {/* <div className="text-[0.5rem]">{JSON.stringify(kiUsMaretPriceDetail)}</div> */}
-            <div className="text-[0.5rem]">{Object.keys(kiUsMaretPriceDetail.output).map((key: any) => { return <div key={key}>{key} : {kiUsMaretPriceDetail.output[key]}</div> })}</div>
+            <div className="text-[0.5rem]">{Object.keys(kiUsMaretPriceDetailOutput).map(key => { return <div key={key}>{key}:{kiUsMaretPriceDetailOutput[key as keyof typeof kiUsMaretPriceDetailOutput]}</div> })}</div>
         </div>
     </>
 }
