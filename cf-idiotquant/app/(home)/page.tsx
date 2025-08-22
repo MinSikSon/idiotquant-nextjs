dc5c5959bec674641d032fd1d0a68be1645cd51b
MinSik Son <MinSikSon@users.noreply.github.com>
2025-08-22 23:43:57 +0900

    [feat] 디자인 통일 (#282)
    Co-authored-by: minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>

--- cf-idiotquant/app/(home)/page.tsx
+++ cf-idiotquant/app/(home)/page.tsx
@@ -7,29 +7,31 @@ import TextType from "@/src/TextAnimations/TextType/TextType";
 
 export default function Home() {
   return <>
-    <div className="dark:border-gray-700 dark:bg-black dark:text-white p-3">
-      <TextType
-        className="text-sm md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl dark:bg-white"
-        text={[
-          "Emotion-Free Quant Investing"
-          , "Stock recommendations driven purely by data."
-          , "idiotquant applies NCAV strategy along with other quantitative indicators."
-          , "It analyzes profitability, undervaluation, and trading volume to automatically spot promising stocks."
-          , "A customized portfolio is then built to match your unique investment style."
-        ]}
-        typingSpeed={75}
-        pauseDuration={1500}
-        showCursor={true}
-        cursorCharacter="|"
-        textColors={[
-          "#000",
-          "#000",
-          "#000",
-          "#000",
-          "#000"
-        ]}
-      />
+    <div className="flex flex-col dark:bg-black h-full dark:text-white">
+      <div className="text-2xl flex items-center justify-left gap-2 p-6 sm:p-8 md:p-10 lg:p-12 ">
+        <TextType
+          className="text-sm md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl dark:bg-white"
+          text={[
+            "Emotion-Free Quant Investing"
+            , "Stock recommendations driven purely by data."
+            , "idiotquant applies NCAV strategy along with other quantitative indicators."
+            , "It analyzes profitability, undervaluation, and trading volume to automatically spot promising stocks."
+            , "A customized portfolio is then built to match your unique investment style."
+          ]}
+          typingSpeed={75}
+          pauseDuration={1500}
+          showCursor={true}
+          cursorCharacter="|"
+          textColors={[
+            "#000",
+            "#000",
+            "#000",
+            "#000",
+            "#000"
+          ]}
+        />
+      </div>
+      <AlgorithmTrade />
     </div>
-    <AlgorithmTrade />
   </>
 }
