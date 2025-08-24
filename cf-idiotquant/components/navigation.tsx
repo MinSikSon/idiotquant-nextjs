a635d3ea9cf16d992993c7fbf2289d1319ef5ffb
MinSik Son <MinSikSon@users.noreply.github.com>
2025-08-24 11:01:40 +0900

    284 task nav bar 한가지 모습으로 통합 (#288)
    * [feat] rotation animation interval 2s -> 5s
    
    * [feat] rotation animation minor fix
    
    * [feat] navigation 개선
    
    ---------
    
    Co-authored-by: minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>

--- cf-idiotquant/components/navigation.tsx
+++ cf-idiotquant/components/navigation.tsx
@@ -55,6 +55,18 @@ export function NavbarWithSimpleLinks() {
                 setOpen(false)
                 setSelectPath(url.split("/")[1]);
             }}>
+                <div className={`border sm:border-none md:border-none lg:border-none px-2 py-1.5 rounded-2xl dark:text-white font-mono text-[0.8rem] hover:bg-gray-100 hover:dark:bg-gray-700 ${selectPath == url.split("/")[1] ? "bg-slate-100 dark:bg-gray-500" : ""} `}>
+                    {label}
+                </div>
+            </Link>
+        );
+    }
+    function NavItemFlexCol({ url, label }: NavItemPropsType) {
+        return (
+            <Link href={url} onClick={() => {
+                setOpen(false)
+                setSelectPath(url.split("/")[1]);
+            }}>
                 <div className={`px-2 py-1.5 rounded-lg dark:text-white font-mono text-[0.8rem] hover:bg-gray-100 hover:dark:bg-gray-700 ${selectPath == url.split("/")[1] ? "bg-slate-100 dark:bg-gray-500" : ""} `}>
                     {label}
                 </div>
@@ -84,6 +96,11 @@ export function NavbarWithSimpleLinks() {
         // "balance-us": <div className={navListDesign}>{!!!kakaoId ? <LockClosedIcon className="h-4 w-4" strokeWidth={2} /> : <LockOpenIcon className="h-4 w-4" strokeWidth={2} />}<WalletIcon className="h-4 w-4" strokeWidth={2} /><div>account inquiry (US)</div></div>,
     }
 
+    const navListUrlToLabelHanburgerButton: any = {
+        "login": <div className={navListDesign}><div>로그인</div> {!!!kakaoId ? <LockClosedIcon className="h-4 w-4" strokeWidth={2} /> : <LockOpenIcon className="h-4 w-4" strokeWidth={2} />}</div>,
+    }
+
+
     const urlToLabel: any = {
         ...navListUrlToLabel,
         // "backtest": `백테스트 ${!!!kakaoId ? "🔒" : ""}`,
@@ -93,7 +110,7 @@ export function NavbarWithSimpleLinks() {
 
     function NavList() {
         return (
-            <div className="flex flex-col md:mb-0 md:mt-0 md:flex-col lg:mb-0 lg:mt-0 lg:flex-col lg:items-left justify-center content-center">
+            <div className="gap-2 flex md:mb-0 md:mt-0 md:flex-col lg:mb-0 lg:mt-0 lg:flex-col lg:items-left justify-center content-center">
                 {Object.keys(navListUrlToLabel).map((key: string) => {
                     return <NavItem key={key} url={`/${key}`} label={navListUrlToLabel[key]} />
                 })}
@@ -101,72 +118,90 @@ export function NavbarWithSimpleLinks() {
         );
     }
 
+    function NavListFlexCol() {
+        return (
+            <div className="flex flex-col md:mb-0 md:mt-0 md:flex-col lg:mb-0 lg:mt-0 lg:flex-col lg:items-left justify-center content-center">
+                {Object.keys(navListUrlToLabelHanburgerButton).map((key: string) => {
+                    return <NavItemFlexCol key={key} url={`/${key}`} label={navListUrlToLabelHanburgerButton[key]} />
+                })}
+            </div>
+        );
+    }
+
     return (
         <>
-            <div className="bg-slate-50 dark:bg-gray-900 dark:text-white flex md:flex-col lg:flex-col items-center justify-start text-blue-gray-900 min-w-56">
-                <div className="p-3 w-full dark:border-gray-600">
-                    <div className="bg-white dark:bg-black flex md:flex-col lg:flex-col border dark:border-gray-600 rounded-lg items-center py-2 w-full">
-                        <Link href="/">
-                            <div className="flex items-center gap-2 px-2 font-mono font-bold cursor-pointer">
-                                idiot
-                                <RotatingText
-                                    texts={['quant', '퀀트', 'quant investing', 'ncav strategy', '순자산가치 전략', 'emotion-free', 'profitability', '수익성', 'undervaluation', '저평가', 'volume', '거래량']}
-                                    mainClassName="px-2 sm:px-2 md:px-3 bg-blue-500 text-white overflow-hidden sm:py-0.5 md:py-1 justify-center rounded-lg"
-                                    staggerFrom={"last"}
-                                    initial={{ y: "100%" }}
-                                    animate={{ y: 0 }}
-                                    exit={{ y: "-120%" }}
-                                    staggerDuration={0.025}
-                                    splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
-                                    transition={{ type: "spring", damping: 30, stiffness: 400 }}
-                                    rotationInterval={2000}
-                                />
-                            </div>
-                        </Link>
-                        {/* <div className="font-mono text-[0.6rem] dark:text-white min-w-32 text-center">
+            <div>
+                <div className="bg-slate-50 dark:bg-gray-900 dark:text-white flex md:flex-col lg:flex-col items-center justify-start text-blue-gray-900 min-w-56">
+                    <div className="p-3 w-full dark:border-gray-600">
+                        <div className="bg-white dark:bg-black flex md:flex-col lg:flex-col border dark:border-gray-600 rounded-lg items-center py-2 w-full">
+                            <Link href="/">
+                                <div className="flex items-center justify-center gap-2 px-2 font-mono font-bold cursor-pointer">
+                                    <div className="pt-0.5">idiot</div>
+                                    <RotatingText
+                                        texts={['quant', '퀀트', 'quant investing', 'ncav strategy', '순자산가치 전략', 'emotion-free', 'profitability', '수익성', 'undervaluation', '저평가', 'volume', '거래량']}
+                                        mainClassName="px-2 sm:px-2 md:px-3 pt-0.5 pt-1 justify-center rounded-md bg-blue-500 text-white overflow-hidden "
+                                        staggerFrom={"last"}
+                                        initial={{ y: "100%" }}
+                                        animate={{ y: 0 }}
+                                        exit={{ y: "-120%" }}
+                                        staggerDuration={0.025}
+                                        splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                                        transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                                        rotationInterval={5000}
+                                    />
+                                </div>
+                            </Link>
+                            {/* <div className="font-mono text-[0.6rem] dark:text-white min-w-32 text-center">
                             {!!kakaoNickName ? <>{kakaoNickName}님 반갑습니다. 😀</>
                                 : <></>
                             }
                         </div> */}
+                        </div>
                     </div>
-                </div>
-                <div className="hidden md:block lg:block w-full">
+                    {/* <div className="hidden md:block lg:block w-full">
                     <div className="flex flex-col w-full p-2 justify-items-center">
                         <div className="flex flex-col p-2 min-h-44 justify-between">
-                            <NavList />
+                            <NavListFlexCol />
                             <ThemeChanger />
                         </div>
                     </div>
-                </div>
-                <div className="md:hidden lg:hidden dark:bg-gray-900 pr-2 py-3">
-                    <DesignButton
-                        handleOnClick={() => handleOpen()}
-                        buttonName={<>
-                            {open ? (
-                                <XMarkIcon className="h-6 w-6" strokeWidth={2} />
-                            ) : (
-                                <Bars3Icon className="h-6 w-6" strokeWidth={2} />
-                            )}
-                        </>}
-                        buttonBgColor="bg-white"
-                        buttonBorderColor="border-gray-500"
-                        buttonShadowColor="#D5D5D5"
-                        textStyle="text-black text-xs font-bold"
-                        buttonStyle={`rounded-lg p-1 flex items-center justify-center mb-2 button bg-white cursor-pointer select-none
+                </div> */}
+                    {/* <div className="md:hidden lg:hidden dark:bg-gray-900 pr-2 py-3">
+                        <DesignButton
+                            handleOnClick={() => handleOpen()}
+                            buttonName={<>
+                                {open ? (
+                                    <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+                                ) : (
+                                    <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+                                )}
+                            </>}
+                            buttonBgColor="bg-white"
+                            buttonBorderColor="border-gray-500"
+                            buttonShadowColor="#D5D5D5"
+                            textStyle="text-black text-xs font-bold"
+                            buttonStyle={`rounded-lg p-1 flex items-center justify-center mb-2 button bg-white cursor-pointer select-none
                         active:translate-y-1 active:[box-shadow:0_0px_0_0_#D5D5D5,0_0px_0_0_#D5D5D541] active:border-[0px]
                         transition-all duration-150 [box-shadow:0_4px_0_0_#D5D5D5,0_8px_0_0_#D5D5D541] border-[1px]
                         `}
-                    />
+                        />
+                    </div> */}
+                </div>
+                <div className="dark:bg-black dark:text-white flex flex-col w-full px-2 justify-items-center">
+                    <div className="dark:bg-black dark:text-white flex flex-col px-2 justify-between">
+                        <NavList />
+                        {/* <ThemeChanger /> */}
+                    </div>
                 </div>
             </div>
-            <Collapse open={open}>
+            {/* <Collapse open={open}>
                 <div className="dark:bg-black dark:text-white flex flex-col w-full p-2 justify-items-center">
                     <div className="dark:bg-black dark:text-white flex flex-col p-2 min-h-44 justify-between">
-                        <NavList />
+                        <NavListFlexCol />
                         <ThemeChanger />
                     </div>
                 </div>
-            </Collapse>
+            </Collapse> */}
         </>
     );
 }
