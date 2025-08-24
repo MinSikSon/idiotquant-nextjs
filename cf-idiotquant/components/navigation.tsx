4414b95e545bc7720c4c6d0c1edb54cfade778e8
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2025-08-24 10:48:40 +0900

    [feat] rotation animation minor fix

--- cf-idiotquant/components/navigation.tsx
+++ cf-idiotquant/components/navigation.tsx
@@ -107,11 +107,11 @@ export function NavbarWithSimpleLinks() {
                 <div className="p-3 w-full dark:border-gray-600">
                     <div className="bg-white dark:bg-black flex md:flex-col lg:flex-col border dark:border-gray-600 rounded-lg items-center py-2 w-full">
                         <Link href="/">
-                            <div className="flex items-center gap-2 px-2 font-mono font-bold cursor-pointer">
-                                idiot
+                            <div className="flex items-center justify-center gap-2 px-2 font-mono font-bold cursor-pointer">
+                                <div className="pt-0.5">idiot</div>
                                 <RotatingText
                                     texts={['quant', '퀀트', 'quant investing', 'ncav strategy', '순자산가치 전략', 'emotion-free', 'profitability', '수익성', 'undervaluation', '저평가', 'volume', '거래량']}
-                                    mainClassName="px-2 sm:px-2 md:px-3 bg-blue-500 text-white overflow-hidden sm:py-0.5 md:py-1 justify-center rounded-lg"
+                                    mainClassName="px-2 sm:px-2 md:px-3 pt-0.5 pt-1 justify-center rounded-md bg-blue-500 text-white overflow-hidden "
                                     staggerFrom={"last"}
                                     initial={{ y: "100%" }}
                                     animate={{ y: 0 }}
