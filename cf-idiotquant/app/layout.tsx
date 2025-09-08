3cf697be87d5d16109249ad9ed965bc04cc3ce49
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2025-09-09 00:11:21 +0900

    [feat] 불필요 코드 주석처리

--- cf-idiotquant/app/layout.tsx
+++ cf-idiotquant/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <StoreProvider>
-      <LoadData />
+      {/* <LoadData /> */}
       <html lang="en"
       // suppressHydrationWarning
       >
