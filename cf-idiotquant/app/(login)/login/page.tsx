5c5cc36be62ccbf9526d78d813996cbad3ef1924
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2025-10-09 07:22:58 +0900

    [bugfix] 장기간 사용하지 않은 경우, 재로그인 필요하지만 로그인 버튼이 안보이는 현상 수정

--- cf-idiotquant/app/(login)/login/page.tsx
+++ cf-idiotquant/app/(login)/login/page.tsx
@@ -80,7 +80,7 @@ export default function LoginPage() {
 
     if (DEBUG) console.log(`[LoginPage] kakaoTotal:`, kakaoTotal, `, undefined == kakaoTotal`, undefined == kakaoTotal);
     const KakaoIcon = () => {
-        if (undefined == kakaoTotal || kakaoTotal?.id == 0 || !!!kakaoTotal?.kakao_account?.profile?.nickname) {
+        if ("cf-need-retry" == loginState || undefined == kakaoTotal || kakaoTotal?.id == 0 || !!!kakaoTotal?.kakao_account?.profile?.nickname) {
             return <>
                 {("cf-login" == loginState) && <LoadKakaoTotal />}
                 <div className="p-5">
