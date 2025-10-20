c8ab21a2e348208b7d8ef13587ee66624ab32571
MinSik Son <MinSikSon@users.noreply.github.com>
2025-10-20 19:44:08 +0900

    [ui] (#335)
    Co-authored-by: minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>

--- cf-idiotquant/components/navigation.tsx
+++ cf-idiotquant/components/navigation.tsx
@@ -89,7 +89,8 @@ export function NavbarWithSimpleLinks() {
                 setOpen(false)
                 setSelectPath(url.split("/")[1]);
             }}>
-                <div className={`px-2 py-1.5 rounded dark:text-white font-mono text-[0.8rem] hover:bg-gray-100 hover:dark:bg-gray-700 ${selectPath == url.split("/")[1] ? "bg-slate-100 dark:bg-gray-500" : ""} `}>
+                {/* <div className={`px-2 py-1.5 rounded dark:text-white font-mono text-[0.8rem] hover:bg-gray-100 hover:dark:bg-gray-700 ${selectPath == url.split("/")[1] ? "bg-slate-100 dark:bg-gray-500" : ""} `}> */}
+                <div>
                     {label}
                 </div>
             </Link>
@@ -210,20 +211,22 @@ export function NavbarWithSimpleLinks() {
                         <DropdownMenu.Root>
                             <DropdownMenu.Trigger>
                                 <Button variant="soft">
-                                    Options
+                                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
                                     <DropdownMenu.TriggerIcon />
                                 </Button>
                             </DropdownMenu.Trigger>
                             <DropdownMenu.Content>
-                                <DropdownMenu.Item shortcut="⌘ E"><NavListFlexCol /></DropdownMenu.Item>
-                                <DropdownMenu.Item shortcut="⌘ D"><ThemeChanger
-                                    handleOpen={handleOpen}
-                                    toggleTheme={toggleTheme}
-                                    setToggleTheme={setToggleTheme}
-                                />
+                                <DropdownMenu.Item shortcut="">
+                                    <NavListFlexCol />
                                 </DropdownMenu.Item>
                                 <DropdownMenu.Separator />
-
+                                <DropdownMenu.Item shortcut="">
+                                    <ThemeChanger
+                                        handleOpen={handleOpen}
+                                        toggleTheme={toggleTheme}
+                                        setToggleTheme={setToggleTheme}
+                                    />
+                                </DropdownMenu.Item>
                             </DropdownMenu.Content>
                         </DropdownMenu.Root>
                     </div>
