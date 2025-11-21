a80264b44707d0fdb9eaae10489a0a1beb473732
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2025-11-21 18:00:28 +0900

    [bugfix] login

--- cf-idiotquant/app/(login)/login/page.tsx
+++ cf-idiotquant/app/(login)/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { setCloudFlareLoginStatus, selectLoginState } from "@/lib/features/login/loginSlice";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
@@ -18,6 +18,8 @@ export default function LoginPage() {
     const loginState = useAppSelector(selectLoginState);
     const kakaoTotal: KakaoTotal = useAppSelector(selectKakaoTotal);
     const kakaoTotalState = useAppSelector(selectKakaoTatalState);
+
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
     useEffect(() => {
         if (DEBUG) console.log(`[LoginPage] loginState:`, loginState);
@@ -53,6 +55,10 @@ export default function LoginPage() {
     }, [kakaoTotalState]);
 
     async function onClickLogin() {
+        if (isSubmitting) return; // 중복 방지
+
+        setIsSubmitting(true);
+
         const redirectUrl = `${process.env.NEXT_PUBLIC_API_URL}/kakao-login`;
         if (DEBUG) console.log(`[LoginPage] onClickLogin`, `redirectUrl:`, redirectUrl);
         const scopeParam = "&scope=friends";
@@ -74,20 +80,23 @@ export default function LoginPage() {
                     <div className="font-mono">
                         login 하려면 아래 버튼을 눌려주세요.
                     </div>
-                    <DesignButton
-                        handleOnClick={() => onClickLogin()}
-                        buttonName="Continue with Kakao"
-                        buttonBgColor="bg-[#ffea04]"
-                        buttonBorderColor="border-[#ebd700]"
-                        buttonShadowColor="#1e1e1e"
-                        textStyle="font-bold text-xs py-2 text-[#3c1e1e]"
-                        additionalTextTop={<img src="/images/kakaotalk_sharing_btn_small.png" alt="metamask" className="h-6 w-6 mx-2" />}
-                        // buttonStyle="mt-6"
-                        buttonStyle={`mt-6 flex items-center justify-center mb-2 px-1 button bg-[#ffea04] rounded-full cursor-pointer select-none
+                    {isSubmitting ? "처리 중..." :
+                        <DesignButton
+                            handleOnClick={() => onClickLogin()}
+                            buttonName="Continue with Kakao"
+                            buttonBgColor="bg-[#ffea04]"
+                            buttonBorderColor="border-[#ebd700]"
+                            buttonShadowColor="#1e1e1e"
+                            textStyle="font-bold text-xs py-2 text-[#3c1e1e]"
+                            additionalTextTop={<img src="/images/kakaotalk_sharing_btn_small.png" alt="metamask" className="h-6 w-6 mx-2" />}
+                            // buttonStyle="mt-6"
+                            buttonStyle={`mt-6 flex items-center justify-center mb-2 px-1 button bg-[#ffea04] rounded-full cursor-pointer select-none
                                 active:translate-y-1 active:[box-shadow:0_0px_0_0_#1e1e1e,0_0px_0_0_#1e1e1e41] active:border-b-[0px]
                                 transition-all duration-150 [box-shadow:0_4px_0_0_#1e1e1e,0_8px_0_0_#1e1e1e41] border-b-[1px]
                                 `}
-                    />
+                        />}
+
+
                 </div>
             </>;
         }
