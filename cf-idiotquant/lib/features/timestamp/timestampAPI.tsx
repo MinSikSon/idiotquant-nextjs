260a39771a5e4ebabb4d4b63a78bbc5fc905225d
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2025-12-03 18:53:36 +0900

    [refac]

--- cf-idiotquant/lib/features/timestamp/timestampAPI.tsx
+++ cf-idiotquant/lib/features/timestamp/timestampAPI.tsx
@@ -4,7 +4,7 @@ export const postTimestamp: any = async (data: any) => {
     const authToken = getCookie("authToken");
     // console.log(`[setLoginStatus] authToken:`, authToken);
 
-    const url = `${process.env.NEXT_PUBLIC_API_URL}/timestamp`
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/timestamp/`
     const options: RequestInit = {
         method: "POST", // GET -> OPTION -> POST
         credentials: "include",  // include credentials (like cookies) in the request
@@ -25,7 +25,7 @@ export const getTimestampList: any = async (count: string) => {
     const authToken = getCookie("authToken");
     // console.log(`[setLoginStatus] authToken:`, authToken);
 
-    const url = `${process.env.NEXT_PUBLIC_API_URL}/timestamp`
+    const url = `${process.env.NEXT_PUBLIC_API_URL}/timestamp/`
     const options: RequestInit = {
         method: "GET", // GET -> OPTION -> POST
         credentials: "include",  // include credentials (like cookies) in the request
