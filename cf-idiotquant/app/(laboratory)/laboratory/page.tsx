0ec8a6b8b67ae029757f96144b26dc400ab995e2
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2025-12-21 22:30:08 +0900

    [bugfix]

--- cf-idiotquant/app/(laboratory)/laboratory/page.tsx
+++ cf-idiotquant/app/(laboratory)/laboratory/page.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 
 // SSR을 비활성화하여 브라우저에서만 컴포넌트를 렌더링합니다.
-const SolarSystem = dynamic(() => import('@/components/solarSystem.tsx'), {
+const SolarSystem = dynamic(() => import('@/components/solarSystem'), {
     ssr: false,
 });
 
