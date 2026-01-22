8007a0f92fd156a323f3f7ce73c52f190dab624a
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2026-01-23 00:32:06 +0900

    [bugfix] build error

--- cf-idiotquant/auth.ts
+++ cf-idiotquant/auth.ts
@@ -2,7 +2,6 @@
 import NextAuth from "next-auth"
 import { authConfig } from "./auth.config"
 import { D1Adapter } from "@auth/d1-adapter"
-import { getRequestContext } from "@cloudflare/next-on-pages";
 
 export const { handlers, auth, signIn, signOut } = NextAuth((req) => {
     // Cloudflare Pages 배포 환경에서 전달되는 env
@@ -17,11 +16,10 @@ export const { handlers, auth, signIn, signOut } = NextAuth((req) => {
     // if (!db) {
     //     console.error("❌ 데이터베이스 바인딩 'db'를 찾을 수 없습니다.");
     // }
-    const { env } = getRequestContext();
 
     return {
         ...authConfig,
-        adapter: D1Adapter(env.db),
+        adapter: D1Adapter(process.env.db),
         trustHost: true, // immutable 에러 완화
     }
 })
