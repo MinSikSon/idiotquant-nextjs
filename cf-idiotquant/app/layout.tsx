31ceb8a2a358871a07058ad3f12913e8b9dec06a
minsikson <sonminsik@sonminsig-ui-MacBook-Pro.local>
2026-02-01 13:46:34 +0900

    [bugfix\ google adsense

--- cf-idiotquant/app/layout.tsx
+++ cf-idiotquant/app/layout.tsx
@@ -9,6 +9,7 @@ import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 // Normalize는 선택사항이지만 Blueprint와 잘 어우러집니다.
 import "normalize.css/normalize.css";
 import { AuthProvider } from "@/components/auth-provider";
+import Script from "next/script";
 
 export const metadata = {
   title: 'idiotquant.com',
@@ -25,6 +26,15 @@ export default function RootLayout({
     // suppressHydrationWarning
     >
       <head>
+        {/* 2. 애드센스 코드 스니펫 주입 */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6995198721227228"
+          crossOrigin="anonymous"
+          strategy="afterInteractive" // 페이지 인터랙션 후 로드 (성능 최적화)
+        />
+        {/* 3. (선택사항) 메타 태그까지 넣어두면 더 확실합니다 */}
+        <meta name="google-adsense-account" content="ca-pub-6995198721227228" />
       </head>
       <body className="md:flex lg:flex bp5-body">
         <StoreProvider>
